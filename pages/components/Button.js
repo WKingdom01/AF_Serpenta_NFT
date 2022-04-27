51da4e2d2053cd369fedb782df528caa7c7487c2
hacryptoha <minatotanaka512@gmail.com>
2022-04-26 19:59:32 -0500

    mint page updates

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 import React, { useState } from 'react';
 
-const Button = ({text, link, style = 'gray'}) =>  {
+const Button = ({text, link, style = 'gray', clickHandler}) =>  {
     const [clicked, setClick] = useState(false)
 
    return ( 
         <div className={`button-container ${style}`}>
         {!link && 
-            <button className={`button button--${clicked ? 'clicked' : ''}`} onMouseDown={() => {setClick(true)}} onMouseUp={() => {setClick(false)}} >
+            <button className={`button button--${clicked ? 'clicked' : ''}`} onMouseDown={() => {setClick(true)}} onMouseUp={() => {setClick(false)}} onClick={clickHandler}>
                 <div className="button__text">
                     {text}
                 </div>
--- pages/components/MintBlock.js
+++ pages/components/MintBlock.js
@@ -1,15 +1,33 @@
 import dynamic from 'next/dynamic'
+import React, { useState, useEffect } from 'react';
 const Button  = dynamic(() =>  import('./Button'))
 const GrayFrame  = dynamic(() =>  import('./GrayFrame'))
 
 
-const MintBlock =() => {          
+const MintBlock =() => {  
+  const [walletConnected, setWalletConnected] = useState(false)
+  
+  const connectWallet = () => {
+    if(!walletConnected) {
+      setWalletConnected(true)
+    }
+  }
   return (
     <div className="mint-block container">
       <GrayFrame title="mint">
         <div className="mint-block__block">
-          <Button link="/" text="Connect Your Wallet"></Button>
-          {/* <Button link="/" text="Mint"></Button> */}
+          {
+            walletConnected ? 
+              <div className="mint-block__container">
+                <div className="mint-block__score">
+                    300/3000 Minted
+
+                </div>
+                <Button link="/" text="Mint"></Button> 
+              
+              </div>
+              : <Button clickHandler={connectWallet} text="Connect Your Wallet"></Button>
+          }
         </div> 
       </GrayFrame>
     </div>
