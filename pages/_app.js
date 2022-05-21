ce4f8583134a7a3fc801f0e7b8214b828294ec9c
hacryptoha <minatotanaka512@gmail.com>
2022-05-20 22:11:15 -0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
 import '../styles/globals.scss'
 import '../styles/reset.scss'
 import '../styles/main.scss'
+import Script from 'next/script';
 import { appWithTranslation } from 'next-i18next'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+  return <>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+    />
+
+    <Script id="gtag-init" strategy="afterInteractive" dangerouslySetInnerHTML={{
+      _html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                console.log(window.location.pathname)
+                                gtag('config', ${gaMeasurementId}, {
+                                    page_path: window.location.pathname,
+                                });
+                            `}}>
+
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default appWithTranslation(MyApp)
