08ad23115065368df7e18e22f73fdea039a2f870
hacryptoha <minatotanaka512@gmail.com>
2022-05-22 13:53:06 -0500

    update roadmap

--- pages/components/Journey.js
+++ pages/components/Journey.js
@@ -6,93 +6,36 @@ import { useTranslation } from 'next-i18next';
 
 
 const Journey = ({ title }) => {
-    const { t } = useTranslation('common')
+    const { t, i18n, ready } = useTranslation('common', { useSuspense: false });
 
 
     return <div className={`journey component js-wrapper`}>
         <div className="journey__content js-slideContainer">
-            <div className="journey__stage">
-                <div className="journey__image">
-                    <Image
-                        className="journey__image"
-                        alt="town"
-                        src="/town.png"
-                        layout="intrinsic"
-                        // object-fit="cover"
-                        width="538"
-                        height="400"
-                        quality={100}
-                        loading="lazy"
-                    />
-                </div>
+            {ready && t('roadmap.stages', { returnObjects: true }).map((stage, index) => {
+                return <div className="journey__stage" key={`stage--${index}`}>
+                    <div className="journey__image">
+                        <Image
+                            className="journey__image"
+                            alt="town"
+                            src={stage.image}
+                            layout="intrinsic"
+                            // object-fit="cover"
+                            width="457"
+                            height="294"
+                            quality={100}
+                            loading="lazy"
+                        />
+                    </div>
 
-                <div className="journey__header">
-                    {t('roadmap.stageOne.header')}
+                    <div className="journey__header">
+                        {stage.header}
+                    </div>
+                    <div className="journey__text">
+                        {stage.text}
+                    </div>
                 </div>
-                <div className="journey__text">
-                    {t('roadmap.stageOne.text')}
-                </div>
-            </div>
-            <div className="journey__stage">
-                <div className="journey__image">
-                    <Image
-
-                        alt="castle"
-                        src="/castle.png"
-                        layout="intrinsic"
-                        width="590"
-                        height="380"
-                        quality={100}
-                        loading="lazy"
-                    />
-                </div>
-                <div className="journey__header">
-                    {t('roadmap.stageTwo.header')}
-                </div>
-                <div className="journey__text">
-                    {t('roadmap.stageTwo.text')}
-                </div>
-            </div>
-            <div className="journey__stage">
-                <   div className="journey__image">
-                    <Image
-                        className="journey__image"
-                        alt="shop"
-                        src="/shop.png"
-                        layout="intrinsic"
-                        width="538"
-                        height="420"
-                        quality={100}
-                        loading="lazy"
-                    />
-                </div>
-                <div className="journey__header">
-                    {t('roadmap.stageThree.header')}
-                </div>
-                <div className="journey__text">
-                    {t('roadmap.stageThree.text')}
-                </div>
-            </div>
-            <div className="journey__stage">
-                <div className="journey__image">
-                    <Image
-                        className="journey__image"
-                        alt="key"
-                        src="/key.png"
-                        layout="intrinsic"
-                        width="538"
-                        height="300"
-                        quality={100}
-                        loading="lazy"
-                    />
-                </div>
-                <div className="journey__header">
-                    {t('roadmap.stageFour.header')}
-                </div>
-                <div className="journey__text">
-                    {t('roadmap.stageFour.text')}
-                </div>
-            </div>
+            })
+            }
         </div>
 
     </div>
