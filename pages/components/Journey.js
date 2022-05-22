e777551cfe0d17a1f3f2e8e0e18e51c2f8b35e37
hacryptoha <minatotanaka512@gmail.com>
2022-05-22 15:55:35 -0500

    mbp size

--- pages/components/Journey.js
+++ pages/components/Journey.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'next-i18next';
 
 
@@ -8,23 +8,59 @@ import { useTranslation } from 'next-i18next';
 const Journey = ({ title }) => {
     const { t, i18n, ready } = useTranslation('common', { useSuspense: false });
 
+    const [shortScreen, setShortScreen] = useState(false)
+
+    useEffect(() => {
+        console.log('yo')
+        if (typeof window !== 'undefined') {
+            // Handler to call on window resize
+            function handleResize() {
+                const bp = window.getComputedStyle(document.body, ':after').content
+
+                if (bp === '"short"') {
+                    setShortScreen(true)
+                } else {
+                    setShortScreen(false)
+                }
+            }
+            window.addEventListener("resize", handleResize);
+
+            // Call handler right away so state gets updated with initial window size
+            handleResize();
+
+            // Remove event listener on cleanup
+            return () => window.removeEventListener("resize", handleResize);
+        }
+    })
 
     return <div className={`journey component js-wrapper`}>
         <div className="journey__content js-slideContainer">
             {ready && t('roadmap.stages', { returnObjects: true }).map((stage, index) => {
                 return <div className="journey__stage" key={`stage--${index}`}>
                     <div className="journey__image">
-                        <Image
+                        {shortScreen ? <Image
                             className="journey__image"
                             alt="town"
                             src={stage.image}
                             layout="intrinsic"
                             // object-fit="cover"
-                            width="457"
-                            height="294"
+                            width="228"
+                            height="147"
                             quality={100}
                             loading="lazy"
                         />
+                            : <Image
+                                className="journey__image"
+                                alt="town"
+                                src={stage.image}
+                                layout="intrinsic"
+                                // object-fit="cover"
+                                width="457"
+                                height="294"
+                                quality={100}
+                                loading="lazy"
+                            />
+                        }
                     </div>
 
                     <div className="journey__header">
