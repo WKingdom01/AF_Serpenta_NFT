c3b2cd58577d0103a8748732a3b71e162a6b0bc4
hacryptoha <minatotanaka512@gmail.com>
2022-07-24 17:34:02 -0600

    update23

--- pages/components/HelpCenter.js
+++ pages/components/HelpCenter.js
@@ -1,11 +1,30 @@
 import Modal from 'react-bootstrap/Modal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import parse, { domToReact }  from "react-html-parser";
 import Image from 'next/image'
 import { useTranslation } from 'next-i18next';
 
 
+const replace = (node) => {
+    if (node.name === 'a') {
+      return (
+        <a {...node.attribs} rel="noreferrer" >
+          { domToReact(node.children) }
+        </a>
+      )
+    }
+  }
 const HelpCenter = ({ modalOpen, setModalOpen }) => {
-
+    // const [_document, set_document] = useState(null)
+    // useEffect(() => {
+    //     set_document(document)
+    // }, [])
+    // const unescapeHTML=(html) => {
+    //     if(_document===null){return;}
+    //     var escapeEl =  _document.createElement("p");
+    //     escapeEl.innerHTML = "<a href='https://medium.com/@miaoux_17900/installing-a-metamask-wallet-for-nfts-7e810e1c1b5c'> https://medium.com/@miaoux_17900/installing-a-metamask-wallet-for-nfts-7e810e1c1b5c</a>";
+    //     return escapeEl;
+    //   }
     const [questionOpen, setQuestionOpen] = useState(0)
 
     const { t, i18n, ready } = useTranslation('common', { useSuspense: false });
@@ -41,7 +60,9 @@ const HelpCenter = ({ modalOpen, setModalOpen }) => {
                     {ready && t('mint.helpCenter.questions', { returnObjects: true }).map((question, index) => {
                         return <div className={`help-center__question ${questionOpen === index + 1 ? 'help-center__question--open' : ''}`} key={`question-hc-${index}`}>
                             <button className="help-center__prompt" onClick={() => { toggleQuestionOpen(index) }}><span className="help-center__chevron">{"> "}</span>{question.prompt}</button>
-                            <div className="help-center__answer">{question.answer}</div>
+                            <div className="help-center__answer" >
+                                {parse(question.answer, { replace })}
+                            </div>
                         </div>
                     })}
                 </div>
