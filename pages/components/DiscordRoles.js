8f63a2fa2959ca8ccb5d1ce0cd68c67c5682f356
hacryptoha <minatotanaka512@gmail.com>
2022-07-27 00:50:24 +0200

    fix: build error

--- pages/components/DiscordRoles.js
+++ pages/components/DiscordRoles.js
@@ -1,19 +1,18 @@
 import { roles } from '../../utils/helpers/roles-color-map';
 
 const DiscordRole = ({ role }) => {
-  if (!role) return;
   return (
     <span className={`discord-role discord-role--${roles[role]}`}>{role}</span>
   );
 };
 
 const DiscordRoles = ({ roles }) => {
-  if (!roles) return;
   return (
     <div className="discord-roles">
-      {roles.map((role, index) => {
-        return <DiscordRole key={index} role={role} />;
-      })}
+      {roles &&
+        roles.map((role, index) => {
+          return <DiscordRole key={index} role={role} />;
+        })}
     </div>
   );
 };
