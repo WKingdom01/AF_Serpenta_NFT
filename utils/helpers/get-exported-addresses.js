5500c5415c2c8eefd32f6027d70f1b6584c5d610
hacryptoha <minatotanaka512@gmail.com>
2022-07-28 23:02:43 +0200

    refactor: use wallets-only json files so we dont expose them

--- utils/helpers/get-exported-addresses.js
+++ utils/helpers/get-exported-addresses.js
@@ -1,16 +1,12 @@
-import whitelistAddresses from '../../public/static/whitelisted-wallets.json';
-import waitlistAddresses from '../../public/static/waitlisted-wallets.json';
+import whitelistAddresses from '../../public/static/whitelisted-wallets-only.json';
+import waitlistAddresses from '../../public/static/waitlisted-wallets-only.json';
 
 /**
  * @function getWhitelistedAddresses
  * @returns {Array[wallet_address]}
  */
 export const getWhitelistedAddresses = () => {
-  let whitelistOnlyAddresses = [];
-  whitelistAddresses.map((item) =>
-    whitelistOnlyAddresses.push(item.wallet_address)
-  );
-  return whitelistOnlyAddresses;
+  return whitelistAddresses;
 };
 
 /**
@@ -18,9 +14,5 @@ export const getWhitelistedAddresses = () => {
  * @returns {Array[wallet_address]}
  */
 export const getWaitlistedAddresses = () => {
-  let waitlistOnlyAddresses = [];
-  waitlistAddresses.map((item) =>
-    waitlistOnlyAddresses.push(item.wallet_address)
-  );
-  return waitlistOnlyAddresses;
+  return waitlistAddresses;
 };
