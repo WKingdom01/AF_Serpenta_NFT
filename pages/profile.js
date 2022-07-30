b5264d6010896d436dbf70c992239b962b9f74ab
hacryptoha <minatotanaka512@gmail.com>
2022-07-30 21:37:19 +0200

    refactor: profile page into separate components

--- pages/profile.js
+++ pages/profile.js
@@ -21,13 +21,172 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 const DiscordRoles = dynamic(() => import('./components/DiscordRoles'));
 const LaunchCountdown = dynamic(() => import('./components/LaunchCountDown'));
 
+const Phases = ({ isWhitelisted, isWaitlisted, isPublic, t }) => {
+  return (
+    <div className={styles.stages}>
+      <div className={styles.stageTxts}>
+        <div className={isWhitelisted ? styles.current : styles.passed}>
+          <span className={styles.list}>{t('profile.phases.0.name')}</span>
+          <span className={styles.amount}>
+            {t('profile.phases.0.description')}
+          </span>
+        </div>
+
+        <div
+          className={
+            isWhitelisted
+              ? styles.upcoming
+              : isWaitlisted
+              ? styles.current
+              : styles.passed
+          }
+        >
+          <span className={styles.list}>
+            <span className={styles.centerTxt}>
+              {t('profile.phases.1.name')}
+            </span>
+          </span>
+          <span className={styles.amount}>
+            <span className={styles.centerTxt}>
+              {t('profile.phases.1.description')}
+            </span>
+          </span>
+        </div>
+
+        <div className={isPublic ? styles.current : styles.upcoming}>
+          <span className={styles.list}>
+            <span className={styles.rightTxt}>
+              {t('profile.phases.2.name')}
+            </span>
+          </span>
+          <span className={styles.amount}>
+            <span className={styles.rightTxt}>
+              {t('profile.phases.2.description')}
+            </span>
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.stageGraph}>
+        <div className={isWhitelisted ? styles.current : styles.passed}>
+          {!isWhitelisted ? <div className={styles.check}></div> : 1}
+        </div>
+        <div
+          className={!isWhitelisted ? styles.passline : styles.timeline}
+        ></div>
+        <div
+          className={
+            isWhitelisted
+              ? styles.upcoming
+              : isWaitlisted
+              ? styles.current
+              : styles.passed
+          }
+        >
+          {isPublic ? <div className={styles.check}></div> : 2}
+        </div>
+        <div className={isPublic ? styles.passline : styles.timeline}></div>
+        <div className={isPublic ? styles.current : styles.upcoming}>3</div>
+      </div>
+    </div>
+  );
+};
+
+const ConnectedWalletInfo = ({
+  discord_username,
+  roles,
+  address,
+  status,
+  whitelisted,
+  t,
+}) => {
+  const { time } = statusHelper(whitelisted);
+
+  return (
+    <div className={styles.infoBox}>
+      <div className={styles.info}>
+        <span className={styles.title}>{t('profile.mintPhaseTitle')}</span>
+        <span className={styles.content}>{time.toString()}</span>
+      </div>
+      <div className={styles.info}>
+        <span className={styles.title}>{t('profile.discordNameTitle')}</span>
+        <span className={styles.content}>{discord_username}</span>
+      </div>
+      <div className={styles.info}>
+        <span className={styles.title}>{t('profile.discordRoleTitle')}</span>
+        <div className={styles.roles}>
+          <DiscordRoles roles={roles} />
+        </div>
+      </div>
+      <div className={styles.info}>
+        <span className={styles.title}>{t('profile.walletAddressTitle')}</span>
+        <span className={styles.content}>{address}</span>
+      </div>
+      <div className={styles.info}>
+        <span className={styles.title}>{t('profile.statusTitle')}</span>
+        <span className={styles.content}>{status}</span>
+      </div>
+    </div>
+  );
+};
+
+const DisconnectWalletInfo = ({ setOpenmodal, t }) => {
+  return (
+    <div className={styles.body}>
+      <div className={styles.img}>
+        <Image src={noConImg} alt="No connected Image" />
+      </div>
+      <div className={styles.title}>{t('profile.disconnectedWalletTitle')}</div>
+      <div className={styles.content}>
+        {t('profile.disconnectedWalletDescription')}
+      </div>
+      <div className={styles.mintbox}>
+        <button className={styles.link} onClick={() => setOpenmodal(true)}>
+          {t('profile.disconnectedWalletButton')}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const LootBoxes = ({ t }) => {
+  return (
+    <div className={styles.lootboxes}>
+      <span className={styles.title}>{t('profile.lootBoxesTitle')}</span>
+      <div className={styles.content}>
+        <i className={styles.number}>2</i>
+      </div>
+    </div>
+  );
+};
+
+const MintBox = ({ distance, t }) => {
+  return (
+    <div className={styles.mintbox}>
+      {distance && (
+        <LaunchCountdown
+          countDownPrefix={t('profile.countdownTitle')}
+          days={distance.days}
+          hours={distance.hours}
+          minutes={distance.minutes}
+          seconds={distance.seconds}
+        />
+      )}
+
+      <Link href="/mint">
+        <a className={styles.link}>{t('profile.countdownButtonText')}</a>
+      </Link>
+    </div>
+  );
+};
+
 export default function Profile() {
   const { t } = useTranslation('common');
   const { isConnected } = useConnect();
   const { data: accountData } = useAccount();
 
   const address = accountData?.address;
-  const { data } = useSwr(`/api/proof/${address}`, fetcher);
+  const { data } = useSwr(address ? `/api/proof/${address}` : null, fetcher);
 
   const [openmodal, setOpenmodal] = useState(false);
   const [distance, setDistance] = useState();
@@ -42,7 +201,6 @@ export default function Profile() {
 
   useEffect(() => {
     if (address && data) {
-      const { time } = statusHelper(whitelisted);
       const { days, hours, minutes, seconds } = getCountdownVariables(time);
 
       setDistance({ days, hours, minutes, seconds });
@@ -57,150 +215,34 @@ export default function Profile() {
         <MintNavBar />
         {isConnected && data ? (
           <div className={styles.body}>
-            <div className={styles.stages}>
-              <div className={styles.stageTxts}>
-                <div className={isWhitelisted ? styles.current : styles.passed}>
-                  <span className={styles.list}>whitelist</span>
-                  <span className={styles.amount}>Mint 3 max</span>
-                </div>
-
-                <div
-                  className={
-                    isWhitelisted
-                      ? styles.upcoming
-                      : isWaitlisted
-                      ? styles.current
-                      : styles.passed
-                  }
-                >
-                  <span className={styles.list}>
-                    <span className={styles.centerTxt}>reserve</span>
-                  </span>
-                  <span className={styles.amount}>
-                    <span className={styles.centerTxt}>Mint 3 max</span>
-                  </span>
-                </div>
-
-                <div className={isPublic ? styles.current : styles.upcoming}>
-                  <span className={styles.list}>
-                    <span className={styles.rightTxt}>public</span>
-                  </span>
-                  <span className={styles.amount}>
-                    <span className={styles.rightTxt}>Unlimited</span>
-                  </span>
-                </div>
-              </div>
-              <div className={styles.stageGraph}>
-                <div className={isWhitelisted ? styles.current : styles.passed}>
-                  {!isWhitelisted ? <div className={styles.check}></div> : 1}
-                </div>
-                <div
-                  className={!isWhitelisted ? styles.passline : styles.timeline}
-                ></div>
-                <div
-                  className={
-                    isWhitelisted
-                      ? styles.upcoming
-                      : isWaitlisted
-                      ? styles.current
-                      : styles.passed
-                  }
-                >
-                  {isPublic ? <div className={styles.check}></div> : 2}
-                </div>
-                <div
-                  className={isPublic ? styles.passline : styles.timeline}
-                ></div>
-                <div className={isPublic ? styles.current : styles.upcoming}>
-                  3
-                </div>
-              </div>
-            </div>
+            <Phases
+              isWhitelisted={isWhitelisted}
+              isWaitlisted={isWaitlisted}
+              isPublic={isPublic}
+              t={t}
+            />
 
             <div className={styles.status}>{alert}</div>
 
-            <div className={styles.infoBox}>
-              <div className={styles.info}>
-                <span className={styles.title}>
-                  {t('profile.mintPhaseTitle')}
-                </span>
-                <span className={styles.content}>{time.toString()}</span>
-              </div>
-              <div className={styles.info}>
-                <span className={styles.title}>
-                  {t('profile.discordNameTitle')}
-                </span>
-                <span className={styles.content}>{discord_username}</span>
-              </div>
-              <div className={styles.info}>
-                <span className={styles.title}>
-                  {t('profile.discordRoleTitle')}
-                </span>
-                <div className={styles.roles}>
-                  {data.roles && <DiscordRoles roles={roles} />}
-                </div>
-              </div>
-              <div className={styles.info}>
-                <span className={styles.title}>
-                  {t('profile.walletAddressTitle')}
-                </span>
-                <span className={styles.content}>{address}</span>
-              </div>
-              <div className={styles.info}>
-                <span className={styles.title}>{t('profile.statusTitle')}</span>
-                <span className={styles.content}>{status}</span>
-              </div>
-            </div>
-            <div className={styles.lootboxes}>
-              <span className={styles.title}>
-                {t('profile.lootBoxesTitle')}
-              </span>
-              <div className={styles.content}>
-                <i className={styles.number}>2</i>
-              </div>
-            </div>
-            <div className={styles.mintbox}>
-              {distance && (
-                <LaunchCountdown
-                  countDownPrefix={t('profile.countdownTitle')}
-                  days={distance.days}
-                  hours={distance.hours}
-                  minutes={distance.minutes}
-                  seconds={distance.seconds}
-                />
-              )}
+            <ConnectedWalletInfo
+              discord_username={discord_username}
+              roles={roles}
+              address={address}
+              status={status}
+              whitelisted={whitelisted}
+              t={t}
+            />
 
-              <Link href="/mint">
-                <a className={styles.link}>
-                  {t('profile.countdownButtonText')}
-                </a>
-              </Link>
-            </div>
+            <LootBoxes t={t} />
+
+            <MintBox distance={distance} t={t} />
           </div>
         ) : (
-          <div className={styles.body}>
-            <div className={styles.img}>
-              <Image src={noConImg} alt="No connected Image" />
-            </div>
-            <div className={styles.title}>
-              {t('profile.disconnectedWalletTitle')}
-            </div>
-            <div className={styles.content}>
-              {t('profile.disconnectedWalletDescription')}
-            </div>
-            <div className={styles.mintbox}>
-              <button
-                className={styles.link}
-                onClick={() => setOpenmodal(true)}
-              >
-                {t('profile.disconnectedWalletButton')}
-              </button>
-            </div>
-          </div>
+          <DisconnectWalletInfo setOpenmodal={setOpenmodal} t={t} />
         )}
-
         <Footer />
       </PageSlot>
+
       <ConnectWallet modalOpen={openmodal} setModalOpen={setOpenmodal} />
     </div>
   );
